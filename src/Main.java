a18b8544a102385f5ecaaadb9587b0b4e01363a7
TimmySK <Karanvirkooner123@outlook.com>
2024-05-24 01:08:21 -0400

    Finished exercise

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,55 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/*
+Title: Random number guesser
+Name: Karanvir Kooner
+Date: 2024-05-24
+Purpose: To familiarize myself with importing libraries and using them, as well as making while loops
+*/
+
+//importing random library
+import java.util.Random;
+//importing scanner library
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        //Setting up scanner
+        Scanner sc = new Scanner(System.in);
+        //Setting up random
+        Random rand = new Random();
+        //Generating a random number
+        int randomNum = rand.nextInt(10);
+        //Creating attempts tally
+        int attempts = 0;
+        //Setting win to false
+        boolean win = false;
+        //Creating while loop for guessing game
+        while (win == false) {
+            //Getting input from user
+            System.out.println("Guess the number: ");
+            //Storing input
+            int guess = sc.nextInt();
+            //Adding attempts per input
+            attempts++;
+            //If statement for users correct guess
+            if (guess == randomNum) {
+                //Sets win to true
+                win = true;
+            }
+            //Else if statement for users guess being too low
+            else if (guess < randomNum) {
+                //Tells user their guess was too low
+                System.out.println("Your guess is too low!");
+            }
+            //Else if statement for users guess being too high
+            else if (guess > randomNum) {
+                //Tells user their guess was too high
+                System.out.println("Your guess is too high!");
+            }
         }
+        //Prints users results
+        System.out.println("YOU WON!");
+        System.out.println("-----Results-----");
+        System.out.println("The number was " + randomNum);
+        System.out.println("It took you " + attempts + " attempts");
     }
 }
