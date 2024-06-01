fdf8b607be607a6aa439079bec45a70b9f23a70f
TimmySK <Karanvirkooner123@outlook.com>
2024-05-31 20:10:35 -0400

    created print statements for user inputs

--- Assignment 1/src/Main.java
+++ Assignment 1/src/Main.java
@@ -11,8 +11,25 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //Setting up scanner
-        Scanner sc = new Scanner(System.in);
 
+        // Setting up scanner
+        Scanner sc = new Scanner(System.in);
+        // Making constant for gravity
+        double GRAVITY = 9.81;
+
+        // Asking user for the initial angle
+        System.out.println("What is the initial angle of the projectile in degrees?: ");
+        // Scanning and storing angle value
+        double angle = sc.nextDouble();
+        // Asking user for the initial velocity
+        System.out.println("What is the initial velocity of the projectile in m/s?: ");
+        // Scanning and storing velocity value
+        double velocity = sc.nextDouble();
+        // Asking user for the time
+        System.out.println("How many seconds have passed?: ");
+        // Scanning and storing time value
+        int seconds = sc.nextInt();
+
+        
     }
 }
