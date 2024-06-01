5d834dff9dcc803e10f2bf3e6fb32c388689abf1
TimmySK <Karanvirkooner123@outlook.com>
2024-05-31 20:50:55 -0400

    Changed class main to assignment1 and validated
    Changed the class from main to assignment 1 because it wouldnt run due to it coexisting with the class exercise file. I also added validation to the inputs to make sure they're positive

---
+++ Assignment 1/src/Assignment1.java
@@ -0,0 +1,49 @@
+/*
+Title: Java console app
+Name: Karanvir Kooner
+Date: 2024-05-31
+Purpose: To create a program that calculate the distance
+in which a projectile is thrown vertically and horizontally
+*/
+
+//importing scanner library
+import java.util.Scanner;
+
+public class Assignment1 {
+    public static void main(String[] args) {
+
+        // Setting up scanner
+        Scanner input = new Scanner(System.in);
+        // Making constant for gravity
+        double GRAVITY = 9.81;
+        // Intialzing variable
+        double angle, velocity, seconds;
+
+        // Asking user for the initial angle
+        do {
+            System.out.println("What is the initial angle of the projectile in degrees?: ");
+            angle = input.nextDouble();
+            if (angle <= 0) {
+                System.out.println("Please enter a positive value for the angle.");
+            }
+        } while (angle <= 0);
+
+        // Asking user for the initial velocity
+        do {
+            System.out.println("What is the initial velocity of the projectile in m/s?: ");
+            velocity = input.nextDouble();
+            if (velocity <= 0) {
+                System.out.println("Please enter a positive value for the velocity.");
+            }
+        } while (velocity <= 0);
+
+        // Asking user for the time
+        do {
+            System.out.println("How many seconds have passed?: ");
+            seconds = input.nextDouble();
+            if (seconds <= 0) {
+                System.out.println("Please enter a positive value for the time.");
+            }
+        } while (seconds <= 0);
+    }
+}
