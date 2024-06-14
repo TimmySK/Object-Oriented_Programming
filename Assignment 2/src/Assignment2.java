e3876ddcf3d92016b0a4991e7604cd83ef340dda
TimmySK <Karanvirkooner123@outlook.com>
2024-06-14 19:46:52 -0400

    Setup scanner and initialized variable(s)

--- Assignment 2/src/Assignment2.java
+++ Assignment 2/src/Assignment2.java
@@ -6,10 +6,15 @@ Purpose: To create a program that will output
 the primes numbers according to the upper bound
 */
 
+// importing scanner library
+import java.util.Scanner;
 
 public class Assignment2 {
     public static void main(String[] args) {
-
+        // setting up scanner
+        Scanner input = new Scanner(System.in);
+        // initializing upper bound
+        int upperBound = 0;
 
     }
 }
