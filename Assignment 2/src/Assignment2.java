1f9b715f1a8463442f06de1a34c23ae0df8784c7
TimmySK <Karanvirkooner123@outlook.com>
2024-06-14 19:42:06 -0400

    Beginning of Assignment 1

---
+++ Assignment 2/src/Assignment2.java
@@ -0,0 +1,15 @@
+/*
+Title: Sieve of Eratosthenes
+Name: Karanvir Kooner
+Date: 2024-06-14
+Purpose: To create a program that will output
+the primes numbers according to the upper bound
+*/
+
+
+public class Assignment2 {
+    public static void main(String[] args) {
+
+
+    }
+}
