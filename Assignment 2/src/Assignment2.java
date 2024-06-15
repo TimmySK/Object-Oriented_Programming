4d0c33c1b9ab71742c0564b12a8fd6758e45311e
TimmySK <Karanvirkooner123@outlook.com>
2024-06-14 21:50:13 -0400

    Fixed validation and added output
    Used try catch method instead for validation, worked a lot better and was an easy change.

--- Assignment 2/src/Assignment2.java
+++ Assignment 2/src/Assignment2.java
@@ -15,38 +15,56 @@ public class Assignment2 {
         Scanner input = new Scanner(System.in);
         // initializing upper bound
         int upperBound = 0;
-        // Asks user for upperbound value
-        System.out.print("Enter an upper bound (2-1000): ");
-        // if statement to verify if the users input is an integer
-        if (input.hasNext()){
-            // saves users input
-            upperBound = input.nextInt();
-            // if statement to verify that users input is 2-1000
-            if (upperBound >=2 && upperBound <= 1000) {
-                // creates a boolean array called prime
-                boolean[] prime = new boolean[upperBound + 1];
-                // for loop to set all numbers from 2 to upperbound as true
-                for (int i = 2; i<= upperBound; i++){
-                    // sets numbers as true
-                    prime[i] = true;
+        // sets validInput for the while loop to false
+        boolean validInput = false;
+        // while statement for validation and output
+        while (!validInput) {
+            // Asks user for upperbound value
+            System.out.print("Enter an upper bound (2-1000): ");
+            // scans input into a string
+            String number = input.next();
+            // try statement to validate input as an integer and between 2-1000
+            try {
+                // parses user input into an integer
+                upperBound = Integer.parseInt(number);
+                // if statement to check if upper bound if between 2-1000
+                if (upperBound >= 2 && upperBound <= 1000) {
+                    validInput = true;
+                } else { // else statement to send user a error if not 2-1000
+                    System.out.println("ERROR! NUMBER MUST BE 2-1000");
                 }
-                // for loop for the sqaure of p as long as it's under the upperbound value
-                for (int p = 2; p * p <= upperBound; p++){
-                    // if statement to check if p is marked as prime
-                    if (prime[p]){
-                        // for loop to mark all multiples as false/not prime
-                        for (int i = p * p; i <= upperBound; i += p){
-                            // sets non-prime numbers as false
-                            prime[i] = false;
-                        }
+            } catch (NumberFormatException e) { // Catch statement to send user an error
+                // prints error message
+                System.out.println("ERROR! INVALID INTEGER");
+            }
+            // creates a boolean array called prime
+            boolean[] prime = new boolean[upperBound + 1];
+            // for loop to set all numbers from 2 to upperbound as true
+            for (int i = 2; i<= upperBound; i++){
+                // sets numbers as true
+                prime[i] = true;
+            }
+            // for loop for the sqaure of p as long as it's under the upperbound value
+            for (int p = 2; p * p <= upperBound; p++){
+                // if statement to check if p is marked as prime
+                if (prime[p]){
+                    // for loop to mark all multiples as false/not prime
+                    for (int i = p * p; i <= upperBound; i += p){
+                        // sets non-prime numbers as false
+                        prime[i] = false;
                     }
                 }
-            } else { // Error message for number not being 2-1000
-                System.out.println("Error! Number must be 2-1000!");
             }
-        } else { // Error message for input not being an integer
-            System.out.println("Error! Must be integer!");
+            // prints header for output
+            System.out.println("These are the prime numbers within the range of " + upperBound + ":");
+            // for loop to continue printing results till upperbound is meet
+            for (int i = 2; i<= upperBound; i++) {
+                // if statement to print all true values
+                if (prime[i]){
+                    // prints prime numbers
+                    System.out.print(i + " ");
+                }
+            }
         }
-
     } // end
 } // end
