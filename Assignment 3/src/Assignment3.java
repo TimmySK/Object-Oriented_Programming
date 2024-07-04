6446c9a49a9d1920fe0de752bed43a09bde77467
TimmySK <Karanvirkooner123@outlook.com>
2024-07-04 14:25:34 -0400

    Beginning assignment 3

---
+++ Assignment 3/src/Assignment3.java
@@ -0,0 +1,18 @@
+/*
+Title: Scoring for Bowling
+Name: Karanvir Kooner
+Date: 2024-07-04
+Purpose: To create a scoring program
+for bowling
+*/
+
+// importing scanner library
+import java.util.Scanner;
+
+public class Assignment3 {
+    public static void main(String[] args) {
+        // setting up scanner
+        Scanner input = new Scanner(System.in);
+
+    } // end
+} // end
