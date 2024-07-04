92e752504325c46f70095ef8010afe582c259ba7
TimmySK <Karanvirkooner123@outlook.com>
2024-07-04 16:32:24 -0400

    Made and tested getTurnScores function

--- Assignment 3/src/Assignment3.java
+++ Assignment 3/src/Assignment3.java
@@ -14,16 +14,23 @@ public class Assignment3 {
         // Setting up Scanner
         Scanner input = new Scanner(System.in);
         // Asking user for playerName
-        System.out.print("What is your name?: ");
-        // Collect playerName
-        String playerName = input.nextLine();
-        // Testing getRoll func
-        int score = getRoll(playerName);
-        System.out.println("---" + playerName + "---\nScore: " + score);
+        System.out.print("Enter player 1s name: ");
+        // Collect player1Name
+        String player1Name = input.nextLine();
+        // Asking user for playerName
+        System.out.print("Enter player 2s name: ");
+        // Collect player2Name
+        String player2Name = input.nextLine();
+        // Testing getTurnScores func
+        int scorePlayer1 = getTurnScores(player1Name);
+        System.out.println("---" + player1Name + "---\nScore: " + scorePlayer1);
+
+        int scorePlayer2 = getTurnScores(player2Name);
+        System.out.println("---" + player2Name + "---\nScore: " + scorePlayer2);
 
     } // end
 
-    public static int getRoll(String playerName) {
+    public static int getRoll(String playerName, int frame, int roll) {
         // Setting up Scanner
         Scanner input = new Scanner(System.in);
         // Setting boolean validInput to false
@@ -34,7 +41,7 @@ public class Assignment3 {
         // While loop for validation
         while (!validInput) {
             // Asks user for players score
-            System.out.print("How much did " + playerName + " score?: ");
+            System.out.print(playerName + "s score in Frame " + frame + ", Roll " + roll + ": ");
             // If statement for validating a integer input
             if (input.hasNextInt()) {
                 // Stores the players score in the variable if check passed
@@ -52,17 +59,59 @@ public class Assignment3 {
                 input.next();
             }
         }
-        // Closes input scanner
-        input.close();
         // Returns score
         return score;
     }
 
-    public static int getTurnScores() {
-
+    public static int getTurnScores(String playerName) {
+        // initialize totalScore as 0
+        int totalScore = 0;
+        // For loop to iterate through the frames
+        for (int frame = 1; frame <= 10; frame++) {
+            // prints a header for the frame
+            System.out.println("---Frame " + frame + "---");
+            // score1 stores the valid score input in the getRoll function
+            int score1 = getRoll(playerName, frame, 1);
+            // score1 gets added to the players totalScore
+            totalScore += score1;
+            // Checks if the player hit a strike
+            if (score1 == 10) {
+                // Displays strike message
+                System.out.println("STRIKE🎳");
+                // score2 stores the valid score input in the getRoll function
+                int score2 = getRoll(playerName, frame, 2);
+                // score2 gets added to the players totalScore
+                totalScore += score2;
+                // If statement for double strike
+                if (score2 == 10) {
+                    // Displays double strike message
+                    System.out.println("🎳Double Strike🎳");
+                    // score3 stores the valid score input in the getRoll function
+                    int score3 = getRoll(playerName, frame, 3);
+                    // score3 gets added to the players totalScore
+                    totalScore += score3;
+                    // If statement for three strikes
+                    if (score3 == 10){
+                        // Displays turkey message
+                        System.out.println("TURKEY🦃");
+                    }
+                }
+            } else { // if not a strike continues like normal
+                int score2 = getRoll(playerName, frame, 2);
+                // score2 gets added to totalScore
+                totalScore += score2;
+                // If statement in case of spare
+                if (score1 + score2 == 10){
+                    // Displays spare message
+                    System.out.println("Spare!!!");
+                    // Allows for an extra roll
+                    totalScore += getRoll(playerName, frame, 3);
+                }
+            }
+        }
+        // Returns totalScore
+        return totalScore;
     }
 
-    public static int[] getGameScores() {
 
-    }
 } // end
