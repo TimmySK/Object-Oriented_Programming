9f3332d628feaf24761b08170fcc673d5bfff847
TimmySK <Karanvirkooner123@outlook.com>
2024-07-26 18:14:06 -0400

    Created PerishableProduct file

---
+++ Assignment 4/src/PerishableProduct.java
@@ -0,0 +1,13 @@
+/*
+Title: Product Class
+Name: Karanvir Kooner
+Date: 2024-07-26
+Purpose: To get and set expiry dates 
+*/
+
+import java.time.LocalDate;
+
+public class PerishableProduct extends Product {
+
+
+}
