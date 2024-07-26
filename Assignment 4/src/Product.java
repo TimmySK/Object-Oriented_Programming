0322b9e209c07ae7a6f1123928db10297d5317ec
TimmySK <Karanvirkooner123@outlook.com>
2024-07-26 16:45:32 -0400

    Made product file

---
+++ Assignment 4/src/Product.java
@@ -0,0 +1,16 @@
+/*
+Title: Product Class
+Name: Karanvir Kooner
+Date: 2024-07-26
+Purpose:
+*/
+
+public class Product {
+    private String SKU;
+    private String productName;
+    private double unitCost;
+    private double salePrice;
+    private int quantityInStore;
+    private int quantityRequired;
+    private String specialInstructions;
+}
