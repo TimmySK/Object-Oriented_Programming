ef8358173f209e26010bc7e55762a7af4fadbc1a
TimmySK <Karanvirkooner123@outlook.com>
2024-07-26 18:41:38 -0400

    Created Main and imported necessary packages

---
+++ Assignment 4/src/Assignment4.java
@@ -0,0 +1,14 @@
+/*
+Title: Main Class
+Name: Karanvir Kooner
+Date: 2024-07-26
+Purpose: To simulate the retail store
+*/
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Assignment4 {
+}
