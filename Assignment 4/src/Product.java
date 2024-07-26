d9cafb4a3f75a7c73fc24d7ea24ec79f4e0f12ee
TimmySK <Karanvirkooner123@outlook.com>
2024-07-26 17:17:22 -0400

    Constructors made

--- Assignment 4/src/Product.java
+++ Assignment 4/src/Product.java
@@ -13,4 +13,27 @@ public class Product {
     private int quantityInStore;
     private int quantityRequired;
     private String specialInstructions;
+
+    // Default constructor
+    public Product() {
+        this.SKU = "DefaultSKU";
+        this.productName = "Default Product";
+        this.unitCost = 0.0;
+        this.salePrice = 0.0;
+        this.quantityInStore = 0;
+        this.quantityRequired = 0;
+        this.specialInstructions = "None";
+    }
+    // Parameterized constructor
+    public Product(String SKU, String productName, double unitCost, double salePrice, int quantityInStore, int quantityRequired, String specialInstructions) {
+        this.SKU = SKU;
+        this.productName = productName;
+        this.unitCost = unitCost;
+        this.salePrice = salePrice;
+        this.quantityInStore = quantityInStore;
+        this.quantityRequired = quantityRequired;
+        this.specialInstructions = specialInstructions;
+    }
+
+
 }
